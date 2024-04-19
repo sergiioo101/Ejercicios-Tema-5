a02e4349bf36da9d0a6162dfaf088aa626b8a000
sergiioo101 <smartros@myuax.com>
2024-04-19 11:04:17 +0200

    10

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -6,4 +6,17 @@ public class Main4 {
         }
         return x * potencia(x, y - 1);
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la base (x): ");
+        int x = scanner.nextInt();
+
+        System.out.print("Ingrese el exponente (y): ");
+        int y = scanner.nextInt();
+
+        System.out.println(x + " elevado a " + y + " es: " + potencia(x, y));
+
+        scanner.close();
+    }
 }
