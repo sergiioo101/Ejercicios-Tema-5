84787afa9ead3bc4f1a294f01b1d3d142b9d853f
sergiioo101 <smartros@myuax.com>
2024-04-19 10:46:48 +0200

    6

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class Main2 {
-    public static void sumaRango (int a, int b) {
+    public static void rangoNum (int a, int b) {
         if (a > b) {
             return;
         }
         System.out.println(a + " ");
-        sumaRango(a + 1, b);
+        rangoNum(a + 1, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el primer numero: ");
+        int a = sc.nextInt();
+        System.out.println("Introduce el segundo numero: ");
+        int b = sc.nextInt();
+        System.out.println("Los números naturales entre " + a + " y " + b + " son:");
+        rangoNum(a, b);
     }
 }
