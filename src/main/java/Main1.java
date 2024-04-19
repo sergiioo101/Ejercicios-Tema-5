8b9dfec8ec72eafb54a67a782f759cbcc10cf500
sergiioo101 <smartros@myuax.com>
2024-04-19 10:36:44 +0200

    3

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,3 +1,11 @@
-public class Main1 {}
+public class Main1 {
+    public static int sumaNaturales(int n) {
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + sumaNaturales(n - 1);
+        }
+    }
+}
 
 
