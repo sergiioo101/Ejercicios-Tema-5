c476a2cce7dca8bddbdabb0df56e924d21ecf4f5
sergiioo101 <smartros@myuax.com>
2024-04-19 11:08:39 +0200

    12

--- src/main/java/Main5.java
+++ src/main/java/Main5.java
@@ -7,4 +7,21 @@ public class Main5 {
         }
         return Math.max(vector[index], maximoVector(vector, index + 1));
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de elementos en el vector: ");
+        int n = scanner.nextInt();
+        int[] vector = new int[n];
+
+        System.out.println("Ingrese los elementos del vector:");
+        for (int i = 0; i < n; i++) {
+            vector[i] = scanner.nextInt();
+        }
+
+        System.out.println("El máximo valor del vector es: " + maximoVector(vector, 0));
+
+        scanner.close();
+    }
 }
